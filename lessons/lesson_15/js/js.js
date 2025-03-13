84cd00b37c8e5dbc147d53371bb10050892c9eee
Drie-Fantom <kkorcixk@gmail.com>
2025-03-13 10:29:16 +0100

    homowork 1+ lesson 15

--- lessons/lesson_15/js/js.js
+++ lessons/lesson_15/js/js.js
@@ -1,65 +1,106 @@
-// let a = true
+// console.log(453453 + 453453)
 
-// console.log(typeof a);
+// console.log('453453' + '453453')
 
-// console.log(23 < 20);
-// console.log(23 <= 20);
-// console.log(23 > 20);
-// console.log(23 >= 20);
 
-// console.log(23 == 20);
-// console.log(23 === 20);
-// console.log(20 !== 20);
+// console.log(typeof 453453)
 
-// -------------------------------------------
-// let num = 0
+// console.log(typeof '453453')
 
-// if (num > 0) {
-//     console.log('posityve number');
-// } else if(num < 0) {
-//     console.log('negative number');
-// } else {
-//     console.log('null');
-// }
 
-// -------------------------------------------
+
+// let firstName = prompt('введите имя')
+// let lastName = prompt('введите фамилие')
+
+
+// let firstName = "Vacho"
+// let lastName = 'Ovasapyan'
+
+// console.log(lastName, firstName);
+
+// firstName = 'Vacho'
+
+// console.log(firstName);
+
+// const age = 23 // const не изменяется
+// console.log(age);
+
+// const r = 12
+// const g = 34 
+// const b = 14
+
+// console.log('rgb(' + r + ', ' + g + ', ' + b + ')');
+
+// console.log(`rgb(${r}, ${g}, ${b})`);
+
+// --------------------------------------------
+// const a = 45
+// const b = '45'
+
+// console.log(+b + +b);
+// console.log(a + a);
+// console.log(a + +b);
+
+
+
+// console.log(45 + +'45');
+// console.log(+'45' + 45);
+
+// console.log('45' + '45');
+
+// console.log('45' - '45');
+// console.log('45' * '45');
+// console.log('45' / '45');
+
+// let num = 'fgfgfg'
+// console.log(num);
+
+// console.log(Number(num));
+
+// let age = prompt('год рождения')
+
+// let date = new Date
+// console.log(data.getFullYear());
+
+
+// let realAge = new Date().getFullYear() - age
+// console.log('вам '+ realAge + ' лет');
+
+// console.log('вам '+ (2025 - age) + ' лет');
+
+// 
+
+// console.log(5 > 4);
+// console.log(5 >= 4);
+
+// console.log(5 < 4);
+// console.log(5 <= 4);
+
+console.log(5 == '5');// true 
+
+console.log('5' === '5'); // false
+
+console.log(5 !== 6)
+
+// -----------------------------------
 // let number = prompt()
 
-// if (number % 2 === 0) {
-//     console.log('четное число');
-// } else if(number % 2 !== 0){
-//     console.log('не четное число');
+// if(number > 0) {
+//     console.log('число положительное');
+// } else if(number < 0){
+//     console.log('число отрицательное');
+// } else if (number === 0) {
+//     console.log('число равно 0');
 // }
 
-// let age = prompt(1700)
 
-// if (age % 400 === 0) {
-//     console.log('високосный год');
-// } else if(age % 100 === 0) {
-//     console.log('не високосный год');
-// } else if(age % 4 === 0) {
-//     console.log('високосный год');
-// } else {
-//     console.log('не високосный год');
-// }
+let number1 = prompt('number 1')
+let number2 = prompt('number 2')
 
-// if (age % 4 === 0 && age % 100 !== 0 || age % 400 === 0) {
-//     console.log('високосный год');
-// } else {
-//     console.log('не високосный год');
-// }
-
-//  - или
-
-// && - и
-const time = 23.99999;
-
-if (time > 5 && time <= 11) {
-    console.log('утро');
-} else if (time > 11 && time <= 17) {
-    console.log('день');
-} else if (time >= 17 && time < 24) {
-    console.log('вечер');
-} else if (time === 24 || time <= 5) {
-    console.log('ночь');
+if (number1 > number2) {
+    console.log(number1 + ' 1 число больше 2');
+} else if (number1 < number2) {
+    console.log(number2 + ' 2 число больше 1');
+} else {
+    console.log(' 2 число равно 1');
 }
